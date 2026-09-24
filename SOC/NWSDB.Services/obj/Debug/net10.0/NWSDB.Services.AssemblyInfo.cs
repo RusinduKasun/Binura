66d1285921af266b0448e70b5f58ea96c73dfd44
Rusindu Kasun <rusindukasundushshantha@gmail.com>
2026-09-24 17:15:17 +0530

    Update styles for improved UI and modify assembly versioning

--- SOC/NWSDB.Services/obj/Debug/net10.0/NWSDB.Services.AssemblyInfo.cs
+++ SOC/NWSDB.Services/obj/Debug/net10.0/NWSDB.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NWSDB.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fdb0594c4cc641ef73a5d83836db003cc874fe5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a468d69f14cbefed41f74d711091354e861d54c")]
 [assembly: System.Reflection.AssemblyProductAttribute("NWSDB.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NWSDB.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
